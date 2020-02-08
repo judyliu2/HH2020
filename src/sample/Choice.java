61c0e593eb8c36867a7590fbe48d2f142e017187
wongRegina <wong.regina.1@gmail.com>
2020-02-08 13:29:22 -0500

    Prompt and Choice classes updated

--- src/sample/Choice.java
+++ src/sample/Choice.java
@@ -1,4 +1,48 @@
 package sample;
 
+import javafx.scene.control.Button;
+
 public class Choice{
+    private String choice;
+    private int health, energy, social, money;
+    private double gpa;
+    private Button button;
+
+    public Choice(String choice, int health, int energy, int social, int money, double gpa) {
+        this.choice = choice;
+        this.health = health;
+        this.energy = energy;
+        this.social = social;
+        this.money = money;
+        this.gpa = gpa;
+        this.button = new Button(choice);
+    }
+
+    public String getChoice() {
+        return choice;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public Button getButton() {
+        return button;
+    }
+
+    public int getEnergy() {
+        return energy;
+    }
+
+    public int getSocial() {
+        return social;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
 }
--- src/sample/Prompt.java
+++ src/sample/Prompt.java
@@ -1,4 +1,19 @@
 package sample;
 
 public class Prompt {
+    private String prompt;
+    private Choice [] choices;
+
+    public Prompt(String prompt, Choice[] choices) {
+        this.prompt = prompt;
+        this.choices = choices;
+    }
+
+    public String getPrompt() {
+        return prompt;
+    }
+
+    public Choice[] getChoices() {
+        return choices;
+    }
 }
